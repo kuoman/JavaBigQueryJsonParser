a9b6efeb926644101319ace32d275caf04401eed
Steve Kuo <kuoman@gmail.comD>
2022-03-07 13:05:41 -0800

    first checkin

---
+++ app/src/test/java/com/industriallogic/bigqueryjsonparser/FizzBuzzTests.java
@@ -0,0 +1,16 @@
+package com.industriallogic.bigqueryjsonparser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class FizzBuzzTests {
+    @Test
+    public void ShouldDoSomething(){
+
+
+        assertThat(3, is(3));
+    }
+
+}
