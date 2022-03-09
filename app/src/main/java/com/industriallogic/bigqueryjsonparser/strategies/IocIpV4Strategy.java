4d8a5c9117691bd22b259bf12bcf2a70a52a99d3
Kody Fintak <kody.fintak@gmail.com>
2022-03-09 17:06:28 -0500

    r Extract keyname constant in IocIpV4Strategy

--- app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/IocIpV4Strategy.java
+++ app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/IocIpV4Strategy.java
@@ -4,16 +4,16 @@ import com.google.gson.JsonObject;
 import com.industriallogic.bigqueryjsonparser.Parse;
 
 public class IocIpV4Strategy {
-    private Parse parse;
+    private final String keyName = "ioc_ip_v4";
+    private final Parse parse;
 
     public IocIpV4Strategy(Parse parse) {
         this.parse = parse;
     }
 
     public JsonObject parse(JsonObject result, JsonObject source) {
-
-        if (source.has("ioc_ip_v4")) {
-            result.add("ioc_ip_v4", parse.getValuesFromArray(source, "ioc_ip_v4"));
+        if (source.has(keyName)) {
+            result.add(keyName, parse.getValuesFromArray(source, keyName));
         }
 
         return result;
