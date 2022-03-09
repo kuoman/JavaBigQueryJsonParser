1ca3de990630068a26c48249cb135e901e875179
Kody Fintak <kody.fintak@gmail.com>
2022-03-09 17:09:34 -0500

    r Move Parse to constructor in SrcIPStrat

--- app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/SrcIpStrategy.java
+++ app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/SrcIpStrategy.java
@@ -1,17 +1,20 @@
 package com.industriallogic.bigqueryjsonparser.strategies;
 
 import com.google.gson.JsonObject;
+import com.industriallogic.bigqueryjsonparser.Parse;
 import com.industriallogic.bigqueryjsonparser.ParseImpl;
 
 public class SrcIpStrategy {
     private final String keyName = "src_ip";
+    private final Parse parse;
 
-    public SrcIpStrategy() {
+    public SrcIpStrategy(Parse parse) {
+        this.parse = parse;
     }
 
     JsonObject parse(JsonObject result, JsonObject source) {
         if (source.has(keyName)) {
-            result.add(keyName, new ParseImpl().extractSubValue(source, keyName));
+            result.add(keyName, parse.extractSubValue(source, keyName));
         }
         return result;
     }
--- app/src/test/java/com/industriallogic/bigqueryjsonparser/strategies/SrcIpStrategyTests.java
+++ app/src/test/java/com/industriallogic/bigqueryjsonparser/strategies/SrcIpStrategyTests.java
@@ -1,13 +1,14 @@
 package com.industriallogic.bigqueryjsonparser.strategies;
 
 import com.google.gson.JsonObject;
+import com.industriallogic.bigqueryjsonparser.ParseImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SrcIpStrategyTests {
-    private final SrcIpStrategy srcIpStrategy = new SrcIpStrategy();
+    private final SrcIpStrategy srcIpStrategy = new SrcIpStrategy(new ParseImpl());
 
     @Test
     public void shouldAddSrcIp() {
