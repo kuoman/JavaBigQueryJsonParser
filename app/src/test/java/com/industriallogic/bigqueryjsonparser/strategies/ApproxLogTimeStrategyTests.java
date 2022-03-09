1fc721bdb6d1d7c52ce53a1895d09d75fd8235f1
Kody Fintak <kody.fintak@gmail.com>
2022-03-09 10:49:23 -0500

    F Should not Extract logtime if does not exist

--- app/src/test/java/com/industriallogic/bigqueryjsonparser/strategies/ApproxLogTimeStrategyTests.java
+++ app/src/test/java/com/industriallogic/bigqueryjsonparser/strategies/ApproxLogTimeStrategyTests.java
@@ -23,8 +23,25 @@ public class ApproxLogTimeStrategyTests {
         assertThat(result.get("approxLogTime").getAsString(), is("1644862497000000"));
     }
 
+    @Test
+    public void shouldNotExtractApproxLogTimeIfDoesNotExist() {
+        // arrange
+        JsonObject source = new JsonObject();
+        source.add("approxLogTime2", new JsonPrimitive(1644862497000L));
+        JsonObject result = new JsonObject();
+
+        // act
+        result = parse(result, source);
+
+        // assert
+        assertThat(result.has("approxLogTime"), is(false));
+    }
+
     private JsonObject parse(JsonObject result, JsonObject source) {
-        result.addProperty("approxLogTime", "1644862497000000");
+        if (source.has("approxLogTime")) {
+            result.addProperty("approxLogTime", "1644862497000000");
+        }
+
         return result;
     }
 }
