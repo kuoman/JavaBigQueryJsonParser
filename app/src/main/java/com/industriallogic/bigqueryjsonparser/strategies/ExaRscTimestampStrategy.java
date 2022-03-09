89bcd88aa553928546c64a157afcf65a36b5e287
Kody Fintak <kody.fintak@gmail.com>
2022-03-09 10:36:56 -0500

    r Extract keyname constant Timestamp

--- app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/ExaRscTimestampStrategy.java
+++ app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/ExaRscTimestampStrategy.java
@@ -4,15 +4,16 @@ import com.google.gson.JsonObject;
 import com.industriallogic.bigqueryjsonparser.Parse;
 
 public class ExaRscTimestampStrategy {
-    private Parse parse;
+    public final String keyname = "exa_rsc.timestamp";
+    private final Parse parse;
 
     public ExaRscTimestampStrategy(Parse parse) {
         this.parse = parse;
     }
 
     public JsonObject parse(JsonObject result, JsonObject source) {
-        if (source.has("exa_rsc.timestamp")) {
-            result.add("exa_rsc.timestamp", parse.getNanoSeconds(source, "exa_rsc.timestamp"));
+        if (source.has(keyname)) {
+            result.add(keyname, parse.getNanoSeconds(source, keyname));
         }
 
         return result;
