af1a1331bdc737ab7e24f2d021ae9283f211f2f6
Kody Fintak <kody.fintak@gmail.com>
2022-03-09 10:52:28 -0500

    r Extract Parse as field

--- app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/ApproxLogTimeStrategy.java
+++ app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/ApproxLogTimeStrategy.java
@@ -4,12 +4,16 @@ import com.google.gson.JsonObject;
 import com.industriallogic.bigqueryjsonparser.Parse;
 
 public class ApproxLogTimeStrategy {
+
+    private Parse parse;
+
     public ApproxLogTimeStrategy() {
+        parse = new Parse();
     }
 
     JsonObject parse(JsonObject result, JsonObject source) {
         if (source.has("approxLogTime")) {
-            result.add("approxLogTime", new Parse().getMicroseconds(source, "approxLogTime"));
+            result.add("approxLogTime", parse.getMicroseconds(source, "approxLogTime"));
         }
 
         return result;
