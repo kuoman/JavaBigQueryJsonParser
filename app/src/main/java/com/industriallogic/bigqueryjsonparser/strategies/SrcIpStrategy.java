17244a689a97fb97dc2196e08d78323441223f03
Kody Fintak <kody.fintak@gmail.com>
2022-03-09 17:08:23 -0500

    r Extract srcIp constant in SrcIpStrat

--- app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/SrcIpStrategy.java
+++ app/src/main/java/com/industriallogic/bigqueryjsonparser/strategies/SrcIpStrategy.java
@@ -4,12 +4,14 @@ import com.google.gson.JsonObject;
 import com.industriallogic.bigqueryjsonparser.ParseImpl;
 
 public class SrcIpStrategy {
+    private final String keyName = "src_ip";
+
     public SrcIpStrategy() {
     }
 
     JsonObject parse(JsonObject result, JsonObject source) {
-        if (source.has("src_ip")) {
-            result.add("src_ip", new ParseImpl().extractSubValue(source, "src_ip"));
+        if (source.has(keyName)) {
+            result.add(keyName, new ParseImpl().extractSubValue(source, keyName));
         }
         return result;
     }
